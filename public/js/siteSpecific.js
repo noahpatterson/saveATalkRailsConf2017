9f48f7413a7b79fede0a4277fe9cf0e1bf1bb0cb
noahpatterson <n@noahpatterson.me>
2017-04-25 07:45:05 -0700

    add mySchedule view

--- public/js/siteSpecific.js
+++ public/js/siteSpecific.js
@@ -8,6 +8,9 @@ $(document).ready(function(){
           db.version(1).stores({
               talks: 'name,speaker,room,track,isSaved,day,startTime,endTime'
           });
+          db.version(2).stores({
+              talks: 'name,speaker,room,track,[isSaved+day],startTime,endTime'
+          });
           loadSavedTalks();
     $('.rc-schedule-talk').click(function() {
         var path = $(this).find('path');
@@ -99,6 +102,7 @@ $(document).ready(function(){
     }
 
     function loadSavedTalks() {
+        //color stars in for saved talks
         db.talks
             .filter(talk => (talk.isSaved == true))
             .toArray()
@@ -106,12 +110,6 @@ $(document).ready(function(){
                 $.each(savedTalks, function(index, value) {
                     var name = value.name;
                     console.log("found saved talk name=" + name);
-
-                    //list saved items
-                    $('#savedItemList').append('<li>day: ' + value.day 
-                    + ' time: ' + value.startTime + ' - ' + value.endTime
-                    + ' talk name: ' + value.name + '</li>');
-
                     $.each($('.rc-schedule-talk__title'), function(index, talk){
                         if (stripWhiteSpace($(talk)) === name) {
                             console.log("talk === name");
@@ -123,36 +121,79 @@ $(document).ready(function(){
                 });
             }).catch(function(error) {
                 console.log(error);
-             //
-             // Finally don't forget to catch any error
-             // that could have happened anywhere in the
-             // code blocks above.
-             //
-             //alert ("Ooops: " + error);
           });
-        }
-//
-          
 
-          //
-          // Put some data into it
-          //
-          //db.friends.put({name: "Nicolas", shoeSize: 8}).then (function(){
-              //
-              // Then when data is stored, read from it
-              //
-              //return db.friends.get('Nicolas');
-          //}).then(function (friend) {
-              //
-              // Display the result
-              //
-              //alert ("Nicolas has shoe size " + friend.shoeSize);
-         /// }).catch(function(error) {
-             //
-             // Finally don't forget to catch any error
-             // that could have happened anywhere in the
-             // code blocks above.
-             //
-             //alert ("Ooops: " + error);
-          //});
+          //populate mySchedule
+          loadMySchedule('tuesday');
+          loadMySchedule('wednesday');
+          loadMySchedule('thursday');
+        }
+
+    function loadMySchedule(dayString) {
+        //tuesday
+        db.talks
+            .filter(talk => (talk.isSaved == true && talk.day == dayString))
+            .toArray()
+            .then(function (savedTalks) {
+                var sortedSavedTalks = savedTalks.sort(function(a, b){
+                    var aStartTimeSplit = [a.startTime.substr(0,a.startTime.length-2),a.startTime.substr(-2)];
+                    var aStartTimeMilitary;
+                    if (aStartTimeSplit[1] === 'AM') {
+                        aStartTimeMilitary = aStartTimeSplit[0];
+                    } else {
+                        var splitColon = aStartTimeSplit[0].split(':');
+                        aStartTimeMilitary = (parseInt(splitColon[0]) + 12) + ":" + splitColon[1] ;
+                    }
+
+                    var bStartTimeSplit = [b.startTime.substr(0,b.startTime.length-2),b.startTime.substr(-2)];
+                    var bStartTimeMilitary;
+                    if (bStartTimeSplit[1] === 'AM') {
+                        bStartTimeMilitary = bStartTimeSplit[0];
+                    } else {
+                        var splitColon = bStartTimeSplit[0].split(':');
+                        bStartTimeMilitary = (parseInt(splitColon[0]) + 12) + ":" + splitColon[1] ;
+                    }
+
+                    return aStartTimeMilitary.localeCompare(bStartTimeMilitary);
+                });
+                $('.myScheduleList').append('<li class="rc-schedule-list-item listItem' + dayString + '"><h3 class="dayListItem">' 
+                        + dayString + '</h3></li>');
+                $.each(savedTalks, function(index, value) {
+                    //list saved items
+                    var listItemDayClass = '.listItem' + dayString;
+                    $(listItemDayClass).append(
+                        '<div class="rc-schedule-talk"><h4 class="rc-schedule-list-item__time">'
+                        + value.startTime + ' - ' + value.endTime + '</h4><div class="rc-schedule-talk__track session__type session__type--security">'
+                        + value.track + '</div><h3 class="rc-schedule-talk__title">'
+                        + value.name  + '</h3><h5 class="rc-schedule-talk__speaker">'
+                        + value.speaker + '</h5><p class="rc-schedule-talk__room">'
+                        + value.room + '</p></div></li>'
+                    );
+                });
+
+            }).catch(function(error) {
+                    console.log(error);
+            });
+        
+                    //     '<li>day: ' + value.day 
+                    // + ' time: ' + value.startTime + ' - ' + value.endTime
+                    // + ' talk name: ' + value.name + '</li>');
+    }
+
+    $('#clearItems').click(deleteAll);
+    $('#refresh').click(refresh);
+
+    function refresh() {
+        location.reload();
+    }
+          
+    function deleteAll() {
+        db.talks
+        .clear()
+        .then(function (deleteCount) {
+            console.log( "Deleted " + deleteCount + " objects");
+            location.reload();
+        });
+        
+    }
 });
