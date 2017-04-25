eaf5d9d5ea678f93e765e228ff7a0a43c3b558b6
noahpatterson <n@noahpatterson.me>
2017-04-25 09:28:17 -0700

    keep trying to fix mobile -- try to use indexed db shim on ios3 - fix shim position

--- public/js/siteSpecific.js
+++ public/js/siteSpecific.js
@@ -7,10 +7,10 @@ $(document).ready(function(){
           //
 
           // This works on all devices/browsers, and uses IndexedDBShim as a final fallback 
-        var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+        // var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
 
-        //   var db = new Dexie("talkSave_database");
-          var db = new Dexie("talkSave_database", {indexedDB: indexedDB});
+          var db = new Dexie("talkSave_database");
+        //   var db = new Dexie("talkSave_database", {indexedDB: indexedDB});
           db.version(1).stores({
               talks: 'name,speaker,room,track,isSaved,day,startTime,endTime'
           });
